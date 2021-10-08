4eaa600492148f4abca502ac6c8dbe69e2b3fb92
rahul-hajare <rahulhajare261997@gmail.com>
2021-10-08 12:33:03 +0530

    Quantity select and add to cart button cart reducer and add to cart add to cart functionality cart screen remove items from cart

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL} from "../constants/productConstants.js"
 
 export const listProducts = () => async (dispatch) => {
-    debugger;
+
     try {
 
         dispatch({type: PRODUCT_LIST_REQUEST})
