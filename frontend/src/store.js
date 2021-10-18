c35142a32f56971f7413b954a9ea54428f322bf8
RahulH7 <rahul@devbeeit.com>
2021-10-18 17:00:19 +0530

    Profile screen and get user details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,14 +3,15 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducers, productDetailsReducers } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import {userLoginReducer, userRegisterReducer} from './reducers/userReducers'
+import {userLoginReducer, userRegisterReducer, userDetailsReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
