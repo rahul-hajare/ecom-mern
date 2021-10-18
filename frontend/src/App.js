25f68fd0bf283d7a6032a0708ee86a00dce9671c
RahulH7 <rahul@devbeeit.com>
2021-10-18 18:58:12 +0530

    shipping screen and save address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/login" component={LoginScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/Cart/:id?" component={CartScreen} />
